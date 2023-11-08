5f3b72a4012683e5c1a3e013fc9d9d510fb5a724
Ricardo Villagrana <ricardovillagranal@gmail.com>
2023-11-08 12:53:18 -0600

    add order placed track event

--- hellotext/inject_script.php
+++ hellotext/inject_script.php
@@ -16,7 +16,6 @@ function hellotext_script () {
 
 add_action( 'wp_head', 'listeners_javascript' );
 function listeners_javascript () {
-    // up one level
     $path = plugin_dir_url( __FILE__ ) . '../javascript/listeners.js';
 
     ?>
@@ -24,3 +23,36 @@ function listeners_javascript () {
     <?php
 }
 
+
+add_action( 'woocommerce_after_order_details', 'order_details_javascript' );
+function order_details_javascript ( $order ) {
+    $products = [];
+
+    foreach ($order->get_items() as $item) {
+        $product = $item->get_product();
+
+        $products[] = [
+            'reference' => $product->id,
+            'type' => 'product',
+            'name' => $product->name,
+            'categories' => wp_get_post_terms( $product->id, 'product_cat', array( 'fields' => 'names' ) ),
+            'currency' => get_woocommerce_currency(),
+            'price' => $product->get_price(),
+            'tags' => wp_get_post_terms( $product->id, 'product_tag', array( 'fields' => 'names' ) ),
+            'image_url' => wp_get_attachment_url( $product->get_image_id() ),
+        ];
+    };
+
+    ?>
+        <script type="module">
+            const order_parameters = {
+                reference: '<?= $order->get_id() ?>',
+                type: 'order',
+                products: <?= wp_json_encode($products) ?>,
+                amount: <?= $order->get_total() ?>,
+                currency: '<?= $order->get_currency() ?>',
+            }
+            Hellotext.track('order.placed', { order_parameters })
+        </script>
+    <?php
+}
