e681e315328e5a3540df588aaa8f835dcf550369
rockwellll <dev.ahmad.khattab@gmail.com>
2025-01-08 10:52:22 +0300

    configure the webchat when has been set

--- src/Misc/Scripts.php
+++ src/Misc/Scripts.php
@@ -6,10 +6,22 @@ add_action( 'wp_head', 'hellotext_script' );
 function hellotext_script () {
 	global $HELLOTEXT_API_URL;
 
-	?>
-		<script type="module">
+    $business_id = get_option('hellotext_business_id');
+    $webchat_id = get_option('hellotext_webchat_id');
+
+    ?>
+        <script type="module">
             import 'https://unpkg.com/@hellotext/hellotext@latest/dist/hellotext.js';
-            Hellotext.initialize('<?php echo esc_html(get_option('hellotext_business_id')); ?>');
-		</script>
-	<?php
+
+            let config = {};
+
+            <?php if ($webchat_id) : ?>
+                config.webChat = {
+                    id: '<?php echo esc_html($webchat_id); ?>'
+                };
+            <?php endif; ?>
+
+            Hellotext.initialize('<?php echo esc_html($business_id); ?>', config);
+        </script>
+    <?php
 }
