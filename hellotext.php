3a5198a822c4fcce45c258a12e8da97ee809320a
rockwellll <dev.ahmad.khattab@gmail.com>
2025-02-04 16:23:19 +0300

    bump version to 1.1.7

--- hellotext.php
+++ hellotext.php
@@ -8,7 +8,7 @@
  * Plugin Name: Hellotext
  * Plugin URI: https://github.com/hellotext/hellotext-wordpress
  * Description: Integrates Hellotext tracking to WooCommerce.
- * Version: 1.1.6
+ * Version: 1.1.7
  * Author: Hellotext
  * Author URI: https://www.hellotext.com
  * License: GPL v2
