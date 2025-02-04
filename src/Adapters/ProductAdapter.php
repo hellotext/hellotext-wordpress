9f02db87dd97dafb6816fdb83c2334cb4f25b336
rockwellll <dev.ahmad.khattab@gmail.com>
2025-02-04 16:11:12 +0300

    remove references of product adapter

--- src/Adapters/ProductAdapter.php
+++ src/Adapters/ProductAdapter.php
@@ -6,11 +6,9 @@ use Hellotext\Adapters\PriceAdapter;
 
 class ProductAdapter {
 	public $product; // WooCommerce product
-	public $item;    // Cart/Order item
 
-	public function __construct ($product, $item = null) {
+	public function __construct ($product) {
 		$this->product = is_numeric($product) ? wc_get_product( $product ) : $product;
-		$this->item = $item;
 	}
 
 	public function get () {
@@ -21,7 +19,6 @@ class ProductAdapter {
 		$response = array(
 			'reference' => $this->product->get_id(),
 			'source' => 'woo',
-			'type' => 'product',
 			'name' => $this->product->get_name(),
 			'categories' => wp_get_post_terms( $this->product->get_id(), 'product_cat', array( 'fields' => 'names' ) ),
 			'price' => ( new PriceAdapter($this->product->get_price()) )->get(),
@@ -29,12 +26,6 @@ class ProductAdapter {
 			'image_url' => wp_get_attachment_url( $this->product->get_image_id() ),
 			'url' => get_permalink( $this->product->get_id() ),
 		);
-
-		if (isset($this->item)) {
-			$response['quantity'] = is_array($this->item)
-				? $this->item['quantity']
-				: $this->item->get_quantity();
-		}
 
 		$response = array_filter($response, function ($value) {
 			return null != $value && [] != $value;
